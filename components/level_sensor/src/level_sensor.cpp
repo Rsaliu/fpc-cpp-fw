54fae9284d8de8dbf82d8b2e6093a7c370c27be4
Tolorunlogo Darasimi <darasimitolorunlogo@gmail.com>
2026-07-26 19:23:15 +0100

    - Level-sensor blinspot correction - Change test for blindspot - Add test for if the read is equal or greater than the blindspot - Add a new error in the common.hpp

--- components/level_sensor/src/level_sensor.cpp
+++ components/level_sensor/src/level_sensor.cpp
@@ -103,7 +103,7 @@ Result<uint16_t> LevelSensor::read()
 
     if (distance_to_sensor <= m_config.blindspot_mm){
         ESP_LOGE(TAG, "The liquid distance is within or below the blindspot");
-        return Result<uint16_t>::err(SystemError::InvalidLength);
+        return Result<uint16_t>::err(SystemError::InvalidLevelReading);
     }
 
     ESP_LOGI(TAG, "[id=%ld] level=%u mm", m_config.id, interp.value());
